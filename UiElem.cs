b8ad3473638b3def57115be7ec1150d42265bd77
Shreddan <40694103+Shreddan@users.noreply.github.com>
2021-02-07 18:04:29 +0000

    It works... sortof why wont it draw though?

--- UiElem.cs
+++ UiElem.cs
@@ -1,16 +1,49 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.GameContent.UI.Elements;
 using Terraria.ID;
-using Terraria.ModLoader;
 using Terraria.UI;
+using Terraria.ModLoader;
+using RPGAdditions.modPlayer;
 
-namespace RPGAdditions.UIEl
+
+namespace RPGAdditions.UI
 {
-    class UiElem : UIState
+    internal class UiElem : UIState
     {
+
+        private UIText text;
+        private UIElement area;
+
+        public override void OnInitialize()
+        {
+            area = new UIElement();
+            area.Left.Set(-area.Width.Pixels - 600, 1f);
+            area.Top.Set(30, 0f);
+            area.Width.Set(182, 0f);
+            area.Height.Set(60, 0f);
+
+            text = new UIText("0/0", 0.8f); // text to show stat
+            text.Width.Set(138, 0f);
+            text.Height.Set(34, 0f);
+            text.Top.Set(40, 0f);
+            text.Left.Set(0, 0f);
+
+            area.Append(text);
+            Append(area);
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            text.SetText($"Level : {Main.LocalPlayer.GetModPlayer<mPlayer>().Level}");
+            this.Update(gameTime);
+        }
     }
 }
--- mPlayer.cs
+++ mPlayer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace RPGAdditions.modPlayer
 {
@@ -14,16 +15,40 @@ namespace RPGAdditions.modPlayer
 		public int Level = 1;
 		public int Exp = 0;
 		public int baseLife = 100;
+		//public int ExpNext = Level * 1.5f + 10;
+
 
 		public void OnLevelUp()
-        {
+		{
 			if (Exp >= Level * 1.5f + 10)
-            {
+			{
 				Level++;
 				Exp = 0;
-				Main.NewText("Congratulations, You have levelled up!  You are now Level" + Main.LocalPlayer.GetModPlayer<mPlayer>().Level);
-            }
-        }
+				player.statLife = player.statLifeMax;
+				Main.NewText("Congratulations, You have levelled up!  You are now Level " + Main.LocalPlayer.GetModPlayer<mPlayer>().Level);
+			}
+		}
+
+		public override void Load(TagCompound tag)
+		{
+			if (tag.ContainsKey("Level"))
+			{
+				Level = tag.GetInt("Level");
+			}
+			if (tag.ContainsKey("Exp"))
+			{
+				Exp = tag.GetInt("Exp");
+			}
+		}
+
+		public override TagCompound Save()
+		{
+			return new TagCompound
+				{
+					{"Level", Level },
+					{"Exp", Exp }
+				};
+		}
 
 
 		public override void PostUpdate()
@@ -37,7 +62,6 @@ namespace RPGAdditions.modPlayer
 				this.player.statLifeMax = (int)(this.baseLife + this.Level * 1.3);
 			}
 			OnLevelUp();
-			base.PostUpdate();
 		}
 	}
 }
